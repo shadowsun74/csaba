1f43d9734dde63adef577e10b3aa8da2b256c711
shadowsun74 <shadowsun@hotmail.com>
2023-01-31 20:59:29 +0100

    Első óra

---
+++ ConsoleApp1/ConsoleApp1/CodeFile6.cs
@@ -0,0 +1,53 @@
+﻿/*
+int szamolo = 0;
+int szam = 0
+int j = i / 3
+
+while (szamolo < 11)
+{
+    if
+        {bool
+
+    }
+    Console.WriteLine(i);
+    i++; // i = i + 1;
+}
+*/
+
+// irjuk ki az elso 10 olyan szamot, ami oszthato 3-mal ( fent sajat. lent 2 megoldas)
+/*
+int i = 1;
+while (i <= 10)
+{
+    Console.WriteLine(i * 3);
+    i++;
+}
+
+int k = 3;
+while (k <= (3* 10))
+{
+    Console.WriteLine(k);
+    k += 3;
+}
+// ezzel is lehet egyszerusiteni. for utan tab-tab behozza az egeszet
+for (int j = 1; j <= 10; j++)
+{
+    Console.WriteLine(j * 3);
+}
+*/
+/*
+int a = 1, b = 1, i = 1;
+while (i <= 10)
+{
+    while (a <= 10)
+    {
+        Console.WriteLine(a * b);
+        a++;
+    }
+    i++;
+}
+*/
+for (int k = 1; k <10; k++)
+{
+    Console.WriteLine(k + " x 1 = " + k);
+}
